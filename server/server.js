2a8d9a2325aadd2077641572ce04c7f5f41aad81
Jitendra2407 <jitendrakumarthakur552@gmail.com>
2024-12-18 00:03:22 +0530

    checking

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Home page")
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
